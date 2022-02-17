62a2e6de298b2e7905004a4ec93e283657bc0cef
p22rlugo <p22rlugo@fairfieldprep.org>
2022-02-17 10:20:40 -0500

    Add file

--- final_prject.py
+++ final_prject.py
@@ -7,8 +7,14 @@
 
 import time
 import random
-from turtle import clear
 
+# Tables for plr and ai to be accessed later for ships
+# 0 = water , 1 = Ship, 2 = Hit
+plrTable = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+aiTable = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+
+# Visible plr Board
+board = []
 
 # Loop var
 loopSelection = True
@@ -23,9 +29,29 @@ Destroyer = 2
 # Ships table so we can compare choice
 ships = ["carter","battleship","cruiser","submarine","destroyer"]
 
+def wait(wait):
+    time.sleep(wait)
+
 def clearConsole():
     print('\n'*150)
 
+def printShips():
+    while len(board) < 100:
+
+        fin = False
+        counter = 0
+
+        for _,var in enumerate(plrTable[counter]):
+            if not fin:
+                fin = True
+                board.append(var)
+                
+                if counter == 10:
+                    counter = 0
+                else:
+                    counter += 1
+                break
+    print(board)
 
 def coin_flip():
     number = random.randint(1,2)
@@ -41,19 +67,28 @@ def shipSelect():
      
     while loopSelection == True:
 
-        time.sleep(4)
+        wait(4)
         clearConsole()
 
         print("Ships left:",ships)
         ship = input("Ship: ")
-        top = int(input("Top Row: "))
-        side = int(input("Side Row: "))
-        rotation = input("Rotate?(Y = yes, N = no): ")
+        try:
+            top = int(input("Top Row: "))
+            side = int(input("Side Row: "))
+            rotation = input("Rotate?(Y = yes, N = no): ")
 
-        for i,v in enumerate(ships):
-            if str.lower(ship) == v: # Checking if they find the ship in the list
-                if top > 0 or side > 0 or str.lower(rotation) != "y" or str.lower(rotation) != "n": # checking if the player met the conditionals
-                    print()
+            for i,v in enumerate(ships):
+                if str.lower(ship) == v: # Checking if they find the ship in the list
+                    if top > 0 or side > 0 or str.lower(rotation) == "y" or str.lower(rotation) == "n": # checking if the player met the conditionals
+                        if plrTable[top-1][side-1] == 0:
+                            plrTable[top-1][side-1] = 1
+                            del ships[i]
+                            printShips()
+                        # del ships[i]
+                    else:
+                        print("Hmm, something you inputted wasn't put in correctly. Try again.")
+        except:
+            print("Hmmm, one of the rows you tried inputting isn't a number. Try again.")
                     
                 
 
@@ -70,13 +105,13 @@ def start_game(startingPosition):
     elif startingPosition == 2:
         clearConsole()
         print("So since you are second, you get to select your ships positions after the AI.")
-        time.sleep(4)
+        wait(4)
         clearConsole()
         print("Waiting for the AI to finish selecting....")
-        time.sleep(10)
+        wait(10)
         clearConsole()
         print("The AI has finished selecting his ships positions, it's now your turn.")
-        time.sleep(4)
+        wait(4)
         if shipSelect() == True:
             loopSelection = False
         
@@ -90,23 +125,23 @@ def user_choice(choice):
         clearConsole()
 
         print("Flipping.")
-        time.sleep(1)
+        wait(1)
         clearConsole()
         print("Flipping..")
-        time.sleep(1)
+        wait(1)
         clearConsole()
         print("Flipping...")
-        time.sleep(2)
+        wait(2)
         clearConsole()
 
         coinflip = coin_flip()
         if coinflip == "Heads":
             print("You got heads so you go first..")
-            time.sleep(2)
+            wait(2)
             start_game(1)
         else:
             print("You got tails so you go second..")
-            time.sleep(2)
+            wait(2)
             start_game(2)
     elif choice == "2":
         print("Credits moment...\nRyan Lugo: Scripter/Designer\nMateo Bonilla: Scripter/Designer\nTeacher: Project Lead")
